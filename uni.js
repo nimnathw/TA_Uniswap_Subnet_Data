6559707a07f5d265c6804debabddb7e5c42fd783
nimnathw <nimnathw@gmail.com>
2023-01-13 07:39:02 +0530

    Added buy signals and sell signals for MACD as points.

--- uni.js
+++ uni.js
@@ -49,44 +49,77 @@ function createChart1(priceUSD) {
 function createChart2(macd, macd_signal, macd_hist) {
   // Create the chart
   const ctx = document.getElementById('myChart2').getContext('2d');
+  
+  // Initialize variables to store buy and sell signal data
+  let buySignals = [];
+  let sellSignals = [];
+  
+  // Iterate through the macd, macd_signal, and macd_hist arrays
+  for (let i = 1; i < macd.length; i++) {
+    // Check if macd crosses above the macd_signal
+    if (macd[i-1] < macd_signal[i-1] && macd[i] > macd_signal[i]) {
+      buySignals.push({x: i, y: macd[i]});
+    }
+    // Check if macd crosses below the macd_signal
+    else if (macd[i-1] > macd_signal[i-1] && macd[i] < macd_signal[i]) {
+      sellSignals.push({x: i, y: macd[i]});
+    }
+  }
+
+
   const chart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: [...Array(macd.length).keys()],
-      datasets: [
-        {
-          label: 'MACD',
-          data: macd,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
-          fill: false
-        },
-        {
-          label: 'MACD Signal',
-          data: macd_signal,
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-          fill: false
-        },
-        {
-          label: 'MACD Histogram',
-          data: macd_hist,
-          borderColor: 'rgba(255, 206, 86, 1)',
-          borderWidth: 1,
-          fill: true,
-          backgroundColor: 'rgba(255, 206, 86, 0.2)'
-        }
-      ]
+        labels: [...Array(macd.length).keys()],
+        datasets: [
+            {
+                label: 'MACD',
+                data: macd,
+                borderColor: 'rgba(128, 0, 128, 1)',
+                borderWidth: 1,
+                fill: false
+            },
+            {
+                label: 'MACD Signal',
+                data: macd_signal,
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+                fill: false
+            },
+            {
+                label: 'MACD Histogram',
+                data: macd_hist,
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1,
+                fill: true,
+                backgroundColor: 'rgba(255, 206, 86, 0.2)'
+            },
+            {
+                label: 'Buy Signals',
+                data: buySignals,
+                borderColor: 'rgba(0, 255, 0, 1)',
+                pointRadius: 10,
+                showLine: false,
+                fill: false
+            },
+            {
+                label: 'Sell Signals',
+                data: sellSignals,
+                borderColor: 'rgba(255, 0, 0, 1)',
+                pointRadius: 10,
+                showLine: false,
+                fill: false
+            }
+        ]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
-  });
-}
-
+  })
+};
